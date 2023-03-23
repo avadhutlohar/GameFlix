7e523d66a1084c8058e08dc16fbb43c7a1df04b8
Avadhut Lohar <loharavadhut@gmail.com>
2023-03-23 22:09:13 +0530

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,25 +1,28 @@
-import { Card, CardBody, Heading,HStack, Image ,Text } from '@chakra-ui/react'
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import CriticScore from './CriticScore'
-import PlatformIconList from './PlatformIconList'
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import PlatformIconList from "./PlatformIconList";
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
