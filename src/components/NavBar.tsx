bcba87c6c21df98e3d84f74dc718597ce6f8a3c8
Avadhut Lohar <loharavadhut@gmail.com>
2023-03-23 20:47:13 +0530

    Build Color Mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,14 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/logo.png';
-
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize='60px' />
-      <Text>NavBar</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-
-export default NavBar
+export default NavBar;
