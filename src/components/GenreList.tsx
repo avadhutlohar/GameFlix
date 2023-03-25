2e089347f524f814e87011d2adfe3a236f10046b
Avadhut Lohar <loharavadhut@gmail.com>
2023-03-25 11:43:45 +0530

    Filtering Games By Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,34 @@
-import React from "react";
-import useGenres from "../hooks/useGeners";
 
-const GenreList = () => {
-  const { data } = useGenres();
+import useGenres, {Genre} from "../hooks/useGeners";
+import { HStack, Image, List, ListItem,Spinner,Button, Text } from "@chakra-ui/react";
+
+import getCroppedImageUrl from "../services/image-url";
+
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
+  const { data ,isLoading ,error} = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
 
   return (
-    <ul>
-      {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
+    <List>
+    {data.map((genre) => (
+      <ListItem key={genre.id} paddingY='5px'>
+        <HStack>
+          <Image
+            boxSize="32px"
+            borderRadius={8}
+            src={getCroppedImageUrl(genre.image_background)}
+          />
+          <Button onClick={()=> onSelectGenre(genre)} fontSize='lg' variant="link">{genre.name}</Button>
+        </HStack>
+      </ListItem>
+    ))}
+  </List>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,6 @@
 import useData from "./useData";
+// @ts-ignore  
+import { Genre } from "./useGenres"
 
 export interface Platform {
   id: number;
@@ -12,6 +14,9 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
